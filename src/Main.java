dda7785bab4f6efc90d714b09cbbeb9f02657314
bolom <bolomozhnov@gmail.com>
2022-11-09 15:28:12 +0300

    massive 1.1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
         // Задача 1,2
 
-        int [] number1 = new int []{1, 2, 3,};
-        number1[0]=1;
-        number1[1]=2;
-        number1[2]=3;
+        int[] number1 = new int[]{1, 2, 3,};
+
         System.out.print(number1[0]);
         System.out.print(",");
         System.out.print(number1[1]);
@@ -16,11 +14,7 @@ public class Main {
         System.out.println();
 
 
-
-        double [] doubleNumber = new double[]{1.57 , 7.654 ,9.986};
-        doubleNumber[0]=1.57;
-        doubleNumber[1]=7.654;
-        doubleNumber[2]=9.986;
+        double[] doubleNumber = new double[]{1.57, 7.654, 9.986};
 
         System.out.print(doubleNumber[0]);
         System.out.print(",");
@@ -30,12 +24,8 @@ public class Main {
         System.out.println();
 
 
+        int[] myMassive = new int[]{4, 5, 6, 7};
 
-        int [] myMassive = new int[]{4 ,5 ,6 ,7};
-        myMassive[0]=4;
-        myMassive[1]=5;
-        myMassive[2]=6;
-        myMassive[3]=7;
         System.out.print(myMassive[0]);
         System.out.print(",");
         System.out.print(myMassive[1]);
@@ -49,10 +39,8 @@ public class Main {
 
         // Задача 3
 
-        number1 = new int []{1, 2, 3,};
-        number1[0]=1;
-        number1[1]=2;
-        number1[2]=3;
+        number1 = new int[]{1, 2, 3,};
+
         System.out.print(number1[2]);
         System.out.print(",");
         System.out.print(number1[1]);
@@ -61,11 +49,8 @@ public class Main {
         System.out.println();
 
 
+        doubleNumber = new double[]{1.57, 7.654, 9.986};
 
-        doubleNumber = new double[]{1.57 , 7.654 ,9.986};
-        doubleNumber[0]=1.57;
-        doubleNumber[1]=7.654;
-        doubleNumber[2]=9.986;
 
         System.out.print(doubleNumber[2]);
         System.out.print(",");
@@ -75,36 +60,31 @@ public class Main {
         System.out.println();
 
 
+        myMassive = new int[]{4, 5, 6, 7};
 
-        myMassive = new int[]{4 ,5 ,6 ,7};
-        myMassive[0]=4;
-        myMassive[1]=5;
-        myMassive[2]=6;
-        myMassive[3]=7;
-        System.out.print(myMassive[3]);
-        System.out.print(",");
-        System.out.print(myMassive[2]);
-        System.out.print(",");
-        System.out.print(myMassive[1]);
-        System.out.print(",");
-        System.out.print(myMassive[0]);
-
-        System.out.println("  ");
-
-
-        // Задача 4
-
-        number1 = new int []{1, 2, 3,};
-        number1[0]=1;
-        number1[1]=2;
-        number1[2]=3;
-
-
-        for (int i = 0; i<3; i=i+1) {
-            System.out.println(number1[i]+ 1);
+        for (int i = 3; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(myMassive[i] + ", ");
+            } else {
+                System.out.print(myMassive[i]);
+            }
         }
 
+            System.out.println();
+
+
+            // Задача 4
+
+            number1 = new int[]{1, 2, 3,};
+
+
+            for (int l = 0; l < 3; l = l + 1) {
+                System.out.print(number1[l] + 1);
+            }
+
+
+        }
 
     }
-}
+
 
